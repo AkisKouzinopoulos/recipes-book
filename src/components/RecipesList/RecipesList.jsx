bd72fde2b0d235859bdf2509818d995a7cbc4b80
Akis Kouzinopoulos <pkouzinopoulos@gmail.com>
2022-06-08 17:55:13 +0100

    finish with adding recipe functionality

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import RecipesListItem from './RecipesListItem/RecipesListItem';
 
-const RecipesList = ({ recipes }) =>
-  recipes.map(recipe => recipe.id && <RecipesListItem key={recipe.id} recipe={recipe} />
-  )
+const RecipesList = ({ recipes }) => recipes.map(recipe => recipe.id && <RecipesListItem key={recipe.id} recipe={recipe} />)
 
 export default RecipesList;
