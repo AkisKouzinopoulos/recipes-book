2106f7b330d2a9863465c01733c20ae831d08128
Akis Kouzinopoulos <pkouzinopoulos@gmail.com>
2022-06-08 12:17:19 +0100

    add different horizontal stepper

--- src/components/DynamicStepper/DynamicStepper.jsx
+++ src/components/DynamicStepper/DynamicStepper.jsx
@@ -1,99 +1,94 @@
 import React, { useContext, useState } from 'react';
-
-// import { withStyles } from '@mui/material/styles';
 import styled from '@emotion/styled';
-
+import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
-import StepButton from '@mui/material/StepButton';
+import StepLabel from '@mui/material/StepLabel';
 import StepContent from '@mui/material/StepContent';
-
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Add from '@mui/icons-material/AddCircle';
-import Delete from '@mui/icons-material/Delete';
+import TextField from '@mui/material/TextField';
 
-import { RecipesContext } from '../../context/Recipes/RecipesContext';
+const steps = [
+  {
+    label: 'Recipe overview',
+    description: `Add recipe overview details`,
+  },
+  {
+    label: 'Add ingredients',
+    description:
+      'An recipe ingredients',
+  },
+  {
+    label: 'Add recipe steps',
+    description: `Add the implementation steps`,
+  },
+];
 
 const DynamicStepper = () => {
+  const [activeStep, setActiveStep] = React.useState(0);
 
-  const [steps, setSteps] = useState([{ title: null, value: null }]);
-  const [activeStep, setActiveStep] = useState();
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
 
-  const { dispatch } = useContext(RecipesContext);
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
-  const addStep = (n = 1) => {
-    const newSteps = steps;
-    // newSteps.push({ title: null, value: '' });
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < n; i++) {
-      newSteps.push({ title: null, value: null });
-    }
-    setSteps(newSteps);
-    setActiveStep(newSteps.length - 1);
-    console.log('ss', steps);
-    dispatch({ type: 'ADD_RECIPE_STEP', payload: { title: 'test title', value: 'value for test' } });
-  }
-
-  const removeStep = (ind) => {
-    if (steps.length < ind + 1) { return; }
-    setSteps(steps.filter((item, i) => i !== ind));
-    // dispatch({ type: 'REMOVE_RECIPE_STEP', payload: steps })
-  }
-
-  const changeStepValue = (index, value) => {
-    steps[index].value = value;
-    // const option = stepOptions.find((item) => item.value === value);
-    // steps[ind].title = option ? option.label : 'Undefined';
-    setSteps(steps);
-    setActiveStep(steps.length);
-  }
+  const handleReset = () => {
+    setActiveStep(0);
+  };
 
   return (
-    <div>
-      <Stepper nonLinear activeStep={activeStep} orientation="vertical">
+    <Box sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={activeStep} orientation="vertical">
         {steps.map((step, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Step key={`step_${index}_container`} >
-            <StepButton
-              onClick={() => setActiveStep(activeStep === index ? steps.length : index)}
+          <Step key={step.label}>
+            <StepLabel
+              optional={
+                index === 2 ? (
+                  <Typography variant="caption">Last step</Typography>
+                ) : null
+              }
             >
-              <div style={{ display: 'flex' }}>
-                <Typography>{step.title || 'Choose an action'}</Typography>
-                {activeStep === index &&
-                  <Delete style={{ marginLeft: 5 }} onClick={() => removeStep(index)} />
-                }
-              </div>
-            </StepButton>
+              {step.label}
+            </StepLabel>
             <StepContent>
-              <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-                <TextField
-                  label="Step description"
-                  id="outlined-size-small"
-                  // defaultValue={`description step: ${index + 1}`}
-                  value={steps[index].value}
-                  onChange={(evt) => changeStepValue(index, evt.target.value)}
-                  size="small"
-                />
-              </FormControl>
+              <Typography>{step.description}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Back
+                  </Button>
+                </div>
+              </Box>
             </StepContent>
           </Step>
         ))}
-        <Step>
-          <StepButton
-            icon={<Add />}
-            onClick={() => addStep()}
-            style={{ fontWeight: 'bold' }}
-          >
-            Add a Step
-          </StepButton>
-        </Step>
       </Stepper>
-    </div>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </Box>
   );
-
 }
 
 export default DynamicStepper;
