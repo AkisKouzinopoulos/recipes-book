aa6f9f59cfdef8f8af6cdb3cda3ddabb44f353bf
Akis <pkouzinopoulos@gmail.com>
2022-06-08 17:57:49 +0100

    Add recipe (#1)
    * add stepper functionality for adding new recipe
    
    * finish with adding recipe functionality

---
+++ src/components/RecipeStepper/RecipeRating/RecipeRating.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useContext } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Typography from '@mui/material/Typography';
+import { RecipesContext } from '../../../context/Recipes/RecipesContext';
+
+const StyledRating = styled(Rating)({
+  '& .MuiRating-iconFilled': {
+    color: '#ff6d75',
+  },
+  '& .MuiRating-iconHover': {
+    color: '#ff3d47',
+  },
+});
+
+const RecipeRating = () => {
+  const [value, setValue] = useState(0);
+
+  const { dispatch } = useContext(RecipesContext);
+
+  const onBlurHandle = () => {
+    dispatch({
+      type: 'UPDATE_NEW_RECIPE', payload: { rating: value }
+    });
+  }
+
+  return (
+    <Box
+      sx={{
+        '& > legend': { mt: 2 },
+      }}
+    >
+      <Typography component="legend">Rate:</Typography>
+      <StyledRating
+        name="customized-color"
+        defaultValue={0}
+        getLabelText={() => `${value} Heart${value !== 1 ? 's' : ''}`}
+        precision={0.5}
+        icon={<FavoriteIcon fontSize="inherit" />}
+        emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onBlur={onBlurHandle}
+      />
+    </Box>
+  )
+};
+
+export default RecipeRating;
--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -1,48 +1,30 @@
 import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import { RECIPES_LIST_PAGE } from "./Paths";
 import { RecipesContext } from '../context/Recipes/RecipesContext';
 import RecipeStepper from '../components/RecipeStepper/RecipeStepper';
 
-const testRecipe = {
-  "title": "Omelete",
-  "description": "Easy omelete",
-  "img": "an-image-url",
-  "totalSteps": 2,
-  "difficulty": "Easy",
-  "rating": 4,
-  "ingredients": [
-    {
-      "ingredient": "eggs",
-      "quantity": "x 2"
-    },
-    {
-      "ingredient": "more eggs",
-      "quantity": "x 4"
-    }
-  ],
-  "steps": [
-    "step one description",
-    "step two description"
-  ]
-}
-
 const AddRecipe = () => {
 
-  // const [newRecipe, setNewRecipe] = useState([]);
   const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
   const { dispatch } = useContext(RecipesContext);
 
   const handleAddRecipe = () => {
     dispatch({ type: 'ADD_RECIPE' });
+    dispatch({ type: 'CLEAR_NEW_RECIPE' });
+    navigate(RECIPES_LIST_PAGE);
   }
 
   return (
-    <>
+    <Box>
       <div>Add Recipe page</div>
       <RecipeStepper />
       <Button onClick={handleAddRecipe}>Add</Button>
-    </>
+    </Box>
   )
 }
 
