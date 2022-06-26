913ce999f5c9e5c1dbfe8bb2f5d51f0e8afa1bf5
Akis Kouzinopoulos <pkouzinopoulos@gmail.com>
2022-06-26 14:09:54 +0100

    create form component and make it reusable

--- src/components/EditRecipeStepper/EditRecipeOverviewStep/EditRecipeOverviewStep.jsx
+++ src/components/EditRecipeStepper/EditRecipeOverviewStep/EditRecipeOverviewStep.jsx
@@ -1,15 +1,9 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import RecipeRating from '../../RecipeRating/RecipeRating';
 import { RecipesContext } from '../../../context/Recipes/RecipesContext';
 import { fetchRecipeDetails } from '../../../context/Recipes/RecipesActions';
+import RecipeOverviewStepForm from '../../RecipeStepper/RecipeOverviewStep/RecipeOverviewStepForm/RecipeOverviewStepForm';
 
 const EditRecipeOverviewStep = () => {
   const { id } = useParams();
@@ -64,64 +58,7 @@ const EditRecipeOverviewStep = () => {
     });
   };
 
-  return (
-    <Stack direction="column" spacing={2}>
-      <TextField
-        required
-        id="outlined-required"
-        label="Title"
-        value={overview.title}
-        name="title"
-        onChange={handleInputChange}
-      />
-      <TextField
-        required
-        id="outlined-multiline-flexible"
-        label="Description"
-        multiline
-        maxRows={4}
-        value={overview.description}
-        name="description"
-        onChange={handleInputChange}
-      />
-      <Stack direction="row" spacing={3}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={overview.difficulty}
-            defaultValue="Easy"
-            label="Difficulty"
-            name="difficulty"
-            onChange={handleInputChange}
-          >
-            <MenuItem value="Easy &amp; fast">Easy &amp; fast</MenuItem>
-            <MenuItem value="Easy">Easy</MenuItem>
-            <MenuItem value="Medium &amp; fast">Medium &amp; fast</MenuItem>
-            <MenuItem value="Medium">Medium</MenuItem>
-            <MenuItem value="Expert &amp; fast">Expert &amp; fast</MenuItem>
-            <MenuItem value="Expert">Expert</MenuItem>
-          </Select>
-        </FormControl>
-        <RecipeRating readonly={false} />
-      </Stack>
-      <TextField
-        id="outlined-prep-time"
-        label="Preparation time"
-        value={overview.preparationTime}
-        name="preparationTime"
-        onChange={handleInputChange}
-      />
-      <TextField
-        id="outlined-img-url"
-        label="Image Url"
-        value={overview.imgUrl}
-        name="imgUrl"
-        onChange={handleInputChange}
-      />
-    </Stack>
-  );
+  return <RecipeOverviewStepForm handleInputChange={handleInputChange} overview={overview} />;
 };
 
 export default EditRecipeOverviewStep;
--- src/components/RecipeStepper/RecipeOverviewStep/RecipeOverviewStep.jsx
+++ src/components/RecipeStepper/RecipeOverviewStep/RecipeOverviewStep.jsx
@@ -1,13 +1,7 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useContext } from 'react';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import RecipeRating from '../../RecipeRating/RecipeRating';
 import { RecipesContext } from '../../../context/Recipes/RecipesContext';
+import RecipeOverviewStepForm from './RecipeOverviewStepForm/RecipeOverviewStepForm';
 
 const RecipeOverviewStep = () => {
   const initialOverview = {
@@ -43,64 +37,7 @@ const RecipeOverviewStep = () => {
     });
   };
 
-  return (
-    <Stack direction="column" spacing={2}>
-      <TextField
-        required
-        id="outlined-required"
-        label="Title"
-        value={overview.title}
-        name="title"
-        onChange={handleInputChange}
-      />
-      <TextField
-        required
-        id="outlined-multiline-flexible"
-        label="Description"
-        multiline
-        maxRows={4}
-        value={overview.description}
-        name="description"
-        onChange={handleInputChange}
-      />
-      <Stack direction="row" spacing={3}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={overview.difficulty}
-            defaultValue="Easy"
-            label="Difficulty"
-            name="difficulty"
-            onChange={handleInputChange}
-          >
-            <MenuItem value="Easy &amp; fast">Easy &amp; fast</MenuItem>
-            <MenuItem value="Easy">Easy</MenuItem>
-            <MenuItem value="Medium &amp; fast">Medium &amp; fast</MenuItem>
-            <MenuItem value="Medium">Medium</MenuItem>
-            <MenuItem value="Expert &amp; fast">Expert &amp; fast</MenuItem>
-            <MenuItem value="Expert">Expert</MenuItem>
-          </Select>
-        </FormControl>
-        <RecipeRating readonly={false} />
-      </Stack>
-      <TextField
-        id="outlined-prep-time"
-        label="Preparation time"
-        value={overview.preparationTime}
-        name="preparationTime"
-        onChange={handleInputChange}
-      />
-      <TextField
-        id="outlined-img-url"
-        label="Image Url"
-        value={overview.imgUrl}
-        name="imgUrl"
-        onChange={handleInputChange}
-      />
-    </Stack>
-  );
+  return <RecipeOverviewStepForm handleInputChange={handleInputChange} overview={overview} />;
 };
 
 export default RecipeOverviewStep;
